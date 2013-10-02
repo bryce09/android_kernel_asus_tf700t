af4606f050c43ddfccf3118c93cf905993befd79
Alex S <alexander.skerhut@gmail.com>
2013-10-02 11:30:22 +0200

    tegra3_clocks: lowest possible clock for g cluster is 204 MHz
    Change-Id: I28b9712115565dbad35fccf5cbf9ad7ff1985e7b

--- arch/arm/mach-tegra/tegra3_clocks.c
+++ arch/arm/mach-tegra/tegra3_clocks.c
@@ -4896,7 +4896,7 @@ static int clip_cpu_rate_limits(
 		return ret;
 	}
 	cpu_clk_lp->max_rate = freq_table[idx].frequency * 1000;
-	cpu_clk_g->min_rate = freq_table[idx-1].frequency * 1000;
+	cpu_clk_g->min_rate = 204 * 1000;
 	data->suspend_index = idx;
 	return 0;
 }
