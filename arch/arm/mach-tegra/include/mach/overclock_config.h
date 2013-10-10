144d8eb1520306c3aab3d7c838b1435704796e47
Alex S <alexander.skerhut@gmail.com>
2013-10-10 17:20:20 +0200

    tegra: overclock_config: add easily adjustable overclock settings
    Change-Id: I51b3b964b49ccee2856fe25a75bc1b968ccf7219

---
+++ arch/arm/mach-tegra/include/mach/overclock_config.h
@@ -0,0 +1,20 @@
+#ifndef _OVERCLOCK_CONFIG_H_
+#define _OVERCLOCK_CONFIG_H_
+
+#define TF700T_CORE_VOLTAGE_CAP        (1425)   /* mV */
+#define TF700T_MIN_CORE_VOLTAGE        (850)    /* mV */
+#define TF700T_MAX_CORE_VOLTAGE        (1425)   /* mV */
+
+#define CORE_VOLTAGE_CAP		(1425)
+#define MIN_CORE_VOLTAGE		(850)
+#define MAX_CORE_VOLTAGE_CAP 		(1425)
+
+#define CORE_FREQUENCY_CAP      (700)    /* MHz */
+#define HOST1X_FREQUENCY_CAP    (350)    /* MHz */
+
+#define TF700T_MAX_CORE_FREQUENCY      (700)    /* MHz */
+#define TF700T_MAX_HOST1X_FREQUENCY    (350)    /* MHz */
+
+#endif
+
+
