314a6d1ed5b81313d4df44f7853b1319d87011ee
Alex S <alexander.skerhut@gmail.com>
2013-12-19 17:25:58 +0100

    hotplug: merge latest franco hotplug needs improvements for tegra 3 e.g. lp mode
    Change-Id: Ic4458123b212baf5c4e518d878e3e64eb6d7e00b

--- arch/arm/kernel/tegra_hotplug.c
+++ arch/arm/kernel/tegra_hotplug.c
@@ -23,207 +23,207 @@
 #include <linux/cpufreq.h>
 #include <linux/delay.h>
 #include <linux/hotplug.h>
-#include <linux/io.h>
-#include <linux/delay.h>
-#include <linux/clk.h>
- 
-#include </usr/src/cm10.1/kernel/asus/tf700t/arch/arm/mach-tegra/pm.h>
+#include <linux/input.h>
+#include <linux/jiffies.h>
+
+
+/*#include </usr/src/cm10.1/kernel/asus/tf700t/arch/arm/mach-tegra/pm.h>
 #include </usr/src/cm10.1/kernel/asus/tf700t/arch/arm/mach-tegra/cpu-tegra.h>
-#include </usr/src/cm10.1/kernel/asus/tf700t/arch/arm/mach-tegra/clock.h>
+#include </usr/src/cm10.1/kernel/asus/tf700t/arch/arm/mach-tegra/clock.h>*/
+
 
 #define DEFAULT_FIRST_LEVEL 60
 #define DEFAULT_CORES_ON_TOUCH 2
 #define HIGH_LOAD_COUNTER 20
 #define TIMER HZ
 
-/*
- * 1000ms = 1 second
- */
-#define MIN_TIME_CPU_ONLINE_MS 1500
+#define MIN_TIME_CPU_ONLINE HZ
 
+extern int *mTouchStatus;
 
-extern int clk_set_parent(struct clk *c, struct clk *parent);
-
-
-static struct clk *cpu_clk;
-static struct clk *cpu_g_clk;
-static struct clk *cpu_lp_clk;
-
-struct cpu_stats
+static struct cpu_stats
 {
-    unsigned int total_cpus;
     unsigned int default_first_level;
-    unsigned int suspend_frequency;
     unsigned int cores_on_touch;
-
     unsigned int counter[2];
-	unsigned long timestamp[2];
+    unsigned long timestamp[2];
+} stats = {
+    .default_first_level = DEFAULT_FIRST_LEVEL,
+    .cores_on_touch = DEFAULT_CORES_ON_TOUCH,
+    .counter = {0},
+        .timestamp = {0},
 };
 
-static struct cpu_stats stats;
+struct cpu_load_data {
+        u64 prev_cpu_idle;
+        u64 prev_cpu_wall;
+};
+
+static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
+
 static struct workqueue_struct *wq;
+static struct workqueue_struct *pm_wq;
 static struct delayed_work decide_hotplug;
+static struct work_struct resume;
+static struct work_struct suspend;
 
-static inline void calc_cpu_hotplug(unsigned int counter0,
-									unsigned int counter1)
+static inline int get_cpu_load(unsigned int cpu)
 {
-	bool online_cpu2 = counter0 >= 10;
-	bool online_cpu3 = counter1 >= 10;
+        struct cpu_load_data *pcpu = &per_cpu(cpuload, cpu);
+        struct cpufreq_policy policy;
+        u64 cur_wall_time, cur_idle_time;
+        unsigned int idle_time, wall_time;
+        unsigned int cur_load;
 
-	if (online_cpu2)
-	{
-		if (cpu_is_offline(2))
-		{
-			cpu_up(2);
-			stats.timestamp[0] = ktime_to_ms(ktime_get());		
-		}
-	}
-	else if (cpu_online(2))
-	{
-		/*
-		 * Let's not unplug this cpu unless its been online for longer than 1
-		 * second to avoid consecutive ups and downs if the load is varying
-		 * closer to the threshold point.
-		 */
-		if (ktime_to_ms(ktime_get()) > MIN_TIME_CPU_ONLINE_MS 
-				+ stats.timestamp[0])
-			cpu_down(2);
-	} 
+        cpufreq_get_policy(&policy, cpu);
 
-	if (online_cpu3)
-	{
-		if (cpu_is_offline(3))
-		{
-			cpu_up(3);
-			stats.timestamp[1] = ktime_to_ms(ktime_get());		
-		}
-	}
-	else if (cpu_online(3))
-	{
-		if (ktime_to_ms(ktime_get()) > MIN_TIME_CPU_ONLINE_MS 
-				+ stats.timestamp[1])
-			cpu_down(3);
-	}
+        cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time, true);
+
+        wall_time = (unsigned int) (cur_wall_time - pcpu->prev_cpu_wall);
+        pcpu->prev_cpu_wall = cur_wall_time;
+
+        idle_time = (unsigned int) (cur_idle_time - pcpu->prev_cpu_idle);
+        pcpu->prev_cpu_idle = cur_idle_time;
+
+        if (unlikely(!wall_time || wall_time < idle_time))
+                return 0;
+
+        cur_load = 100 * (wall_time - idle_time) / wall_time;
+
+        return (cur_load * policy.cur) / policy.max;
 }
 
-static void decide_hotplug_func(struct work_struct *work)
+static void cpu_revive(unsigned int cpu)
+{
+        cpu_up(cpu);
+        stats.timestamp[cpu - 2] = jiffies;
+}
+
+static void cpu_smash(unsigned int cpu)
+{
+        /*
+         * Let's not unplug this cpu unless its been online for longer than
+         * 1sec to avoid consecutive ups and downs if the load is varying
+         * closer to the threshold point.
+         */
+        if (time_is_after_jiffies(stats.timestamp[cpu - 2] + MIN_TIME_CPU_ONLINE))
+                return;
+
+        cpu_down(cpu);
+        stats.counter[cpu - 2] = 0;
+}
+
+static void __ref decide_hotplug_func(struct work_struct *work)
 {
     int cpu;
-    
-
-#if 0
-    if (unlikely(is_touching && num_online_cpus() < stats.cores_on_touch))
-    {
-        for_each_possible_cpu(cpu_boost)
+        int i;
+        int cpu_nr = 2;
+        unsigned int cur_load;
+        
+        if (mTouchStatus[0] != 0)
         {
-            if (!cpu_online(cpu_boost) && cpu_boost < stats.cores_on_touch) 
-            {
-                cpu_up(cpu_boost);
-            }
+                for (i = num_online_cpus(); i < stats.cores_on_touch; i++)
+                {
+                        if (cpu_is_offline(i))
+                        {
+                                cpu_up(i);
+                                stats.timestamp[i-2] = ktime_to_ms(ktime_get());
+                        }
+                }
+				pr_info("Touch detected reque\n");
+                goto re_queue;
         }
-    }
-#endif
+
     for_each_online_cpu(cpu) 
     {
-        if (report_load_at_max_freq(cpu) >= stats.default_first_level)
-        {
-            if (likely(stats.counter[cpu] < HIGH_LOAD_COUNTER))    
-                stats.counter[cpu] += 2;
+                cur_load = get_cpu_load(cpu);
+
+                if (cur_load >= stats.default_first_level)
+                {
+                        if (likely(stats.counter[cpu] < HIGH_LOAD_COUNTER))    
+                                stats.counter[cpu] += 2;
+
+                        if (cpu_is_offline(cpu_nr) && stats.counter[cpu] >= 10)
+                                cpu_revive(cpu_nr);
+                }
+
+                else
+                {
+                        if (stats.counter[cpu])
+                                --stats.counter[cpu];
+
+                        if (cpu_online(cpu_nr) && stats.counter[cpu] < 10)
+                                cpu_smash(cpu_nr);
+                }
+
+                cpu_nr++;
+
+                if (cpu)
+                        break;
         }
 
-        else
-        {
-            if (stats.counter[cpu] > 0)
-                stats.counter[cpu]--;
-        }
-
-        if (cpu) 
-        {
-            cpu = 0;
-            break;
-        }
-    }
-
-	calc_cpu_hotplug(stats.counter[0], stats.counter[1]);
-	
-	
-#ifdef DEBUG_HOTPLUG
-	pr_info("HOTPLUG DEBUG\n");
-	pr_info("0: stats.counter:\t%d", stats.counter[0]);
-	pr_info("1: stats.counter:\t%d", stats.counter[1]);
-	//pr_info("load_avg 0,1:\t%d", load_avg);
-#endif
+re_queue:        
     queue_delayed_work(wq, &decide_hotplug, msecs_to_jiffies(TIMER));
 }
 
-static void mako_hotplug_early_suspend(struct early_suspend *handler)
-{	 
-    int cpu;
+static void suspend_func(struct work_struct *work)
+{
+        int cpu;
 
     /* cancel the hotplug work when the screen is off and flush the WQ */
+        flush_workqueue(wq);
     cancel_delayed_work_sync(&decide_hotplug);
-    flush_workqueue(wq);
+        cancel_work_sync(&resume);
 
     pr_info("Early Suspend stopping Hotplug work...\n");
     
     for_each_online_cpu(cpu) 
     {
         if (cpu) 
-        {
             cpu_down(cpu);
-        }
     }
-	
-	/* limit max frequency in order to enter lp mode */
-	tegra_update_cpu_speed(475*1000);
-	if(!clk_set_parent(cpu_clk, cpu_lp_clk)) 
-	{
-		/*catch-up with governor target speed */
-		tegra_cpu_set_speed_cap(NULL);
-		/* process pending core requests*/
-#ifdef DEBUG_HOTPLUG
-		pr_info("Running Lp core\n");
-#endif
-	}
+
+        /* reset the counters so that we start clean next time the display is on */
+    stats.counter[0] = 0;
+    stats.counter[1] = 0;
+     
 }
 
+static void __ref resume_func(struct work_struct *work)
+{
+        int cpu;
+
+        cancel_work_sync(&suspend);
+
+       
+    pr_info("Cpulimit: Late resume - restore cpu%d max frequency.\n", 0);
+
+    /* online all cores when the screen goes online */
+    for_each_possible_cpu(cpu) 
+    {
+        if (cpu) 
+            cpu_up(cpu);
+    }
+    
+    pr_info("Late Resume starting Hotplug work...\n");
+    queue_delayed_work(wq, &decide_hotplug, HZ * 2);        
+}
+
+static void mako_hotplug_early_suspend(struct early_suspend *handler)
+{         
+    queue_work(pm_wq, &suspend);
+}
 
 static void mako_hotplug_late_resume(struct early_suspend *handler)
 {  
-    int cpu;
-	
-	if(!clk_set_parent(cpu_clk, cpu_g_clk)) 
-	{
-		/*catch-up with governor target speed */
-		tegra_cpu_set_speed_cap(NULL);
-		/* process pending core requests*/
-#ifdef DEBUG_HOTPLUG
-		pr_info("Running G Cluster\n");
-#endif
-	}
-	
-	msleep(70);
-    /* online 2 cores when the screen goes online */
-    for(cpu = 0; cpu < 2; cpu++) 
-    {
-        if (!cpu_online(cpu)) 
-        {
-            cpu_up(cpu);
-        }
-    }
-
-    stats.counter[0] = 0;
-    stats.counter[1] = 0;
-
-    pr_info("Late Resume starting Hotplug work...\n");
-    queue_delayed_work(wq, &decide_hotplug, HZ);
+        queue_work(pm_wq, &resume);
 }
 
 static struct early_suspend mako_hotplug_suspend =
 {
     .level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 1,
-	.suspend = mako_hotplug_early_suspend,
-	.resume = mako_hotplug_late_resume,
+        .suspend = mako_hotplug_early_suspend,
+        .resume = mako_hotplug_late_resume,
 };
 
 /* sysfs functions for external driver */
@@ -232,10 +232,6 @@ void update_first_level(unsigned int level)
     stats.default_first_level = level;
 }
 
-void update_suspend_frequency(unsigned int freq)
-{
-    stats.suspend_frequency = freq;
-}
 
 void update_cores_on_touch(unsigned int num)
 {
@@ -247,6 +243,7 @@ unsigned int get_first_level()
     return stats.default_first_level;
 }
 
+
 unsigned int get_cores_on_touch()
 {
     return stats.cores_on_touch;
@@ -255,30 +252,24 @@ unsigned int get_cores_on_touch()
 
 int __init mako_hotplug_init(void)
 {
-	pr_info("Mako Hotplug driver started.\n");
+        pr_info("Mako Hotplug driver started.\n");
 
-	cpu_clk = clk_get_sys(NULL, "cpu");
-	cpu_g_clk = clk_get_sys(NULL, "cpu_g");
-	cpu_lp_clk = clk_get_sys(NULL, "cpu_lp");
-
-	if (NULL ==(cpu_clk) || (cpu_g_clk)==NULL || (cpu_lp_clk)==NULL)
-	{
-		pr_info("Error getting cpu clocks....");
-		return -ENOENT;
-	}
-    /* init everything here */
-    stats.total_cpus = num_present_cpus();
-    stats.default_first_level = DEFAULT_FIRST_LEVEL;
-    stats.cores_on_touch = DEFAULT_CORES_ON_TOUCH;
-    stats.counter[0] = 0;
-    stats.counter[1] = 0;
-
-    wq = alloc_workqueue("mako_hotplug_workqueue", WQ_UNBOUND | WQ_RESCUER | WQ_FREEZABLE, 1);
+    wq = alloc_ordered_workqueue("mako_hotplug_workqueue", 0);
     
     if (!wq)
         return -ENOMEM;
+
+        pm_wq = alloc_workqueue("pm_workqueue", 0, 1);
     
+    if (!pm_wq)
+        return -ENOMEM;
+
+        stats.timestamp[0] = jiffies;
+        stats.timestamp[1] = jiffies;    
+
     INIT_DELAYED_WORK(&decide_hotplug, decide_hotplug_func);
+        INIT_WORK(&resume, resume_func);
+        INIT_WORK(&suspend, suspend_func);
     queue_delayed_work(wq, &decide_hotplug, HZ*25);
     
     register_early_suspend(&mako_hotplug_suspend);
@@ -286,5 +277,3 @@ int __init mako_hotplug_init(void)
     return 0;
 }
 late_initcall(mako_hotplug_init);
-
-
